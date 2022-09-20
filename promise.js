dfdc4676488076a04603c81aff81ab973dbe92ba
herrysabar <sabarudin.herry@gmail.com>
2022-09-20 08:37:39 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,14 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput =async(hasil)  => {
-  return new Promise((resolve, reject) => {
-    if (hasil == 'marah'){
-      resolve('2')
-    } else {
-      reject('4')
-    };
-  });
+const promiseOutput = async (stat) => {
+    var x =0;
+    await promiseTheaterIXX().then((value) => 
+    {
+        value.forEach(emosi => {
+            if (emosi.hasil==stat)
+            {
+                x+=1;
+            }
+        });
+    });
+    await promiseTheaterVGC().then((value) => 
+    {
+        value.forEach(emosi => {
+            if (emosi.hasil==stat)
+            {
+                x+=1;
+            }
+        });
+    });
+    return x;
 };
 
 module.exports = {
